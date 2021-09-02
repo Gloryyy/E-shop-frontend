b5988b304dd103e43e08c6797f5b43ccbb81c7c9
gloryyy <majdwarrrak@gmail.com>
2021-09-03 00:49:54 +0200

    deploy our server to heroku

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -1,12 +1,14 @@
 import { Platform } from 'react-native';
 
-let baseURL = '';
+let baseURL = 'https://easy-shop-server0.herokuapp.com/api/v1/';
+
+/* let baseURL = '';
 
 {
   Platform.OS == 'android'
     ? (baseURL =
         'http://6102-2a01-c23-752b-be00-3484-f495-ddc1-446b.ngrok.io/api/v1/')
     : (baseURL = 'http://localhost:3000/api/v1/');
-}
+} */
 
 export default baseURL;
