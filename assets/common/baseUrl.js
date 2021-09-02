f32d7a2144156bb13c8223cee48b658c960ae616
gloryyy <majdwarrrak@gmail.com>
2021-09-03 00:51:27 +0200

    first commit

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -7,7 +7,7 @@ let baseURL = 'https://easy-shop-server0.herokuapp.com/api/v1/';
 {
   Platform.OS == 'android'
     ? (baseURL =
-        'http://6102-2a01-c23-752b-be00-3484-f495-ddc1-446b.ngrok.io/api/v1/')
+        'http://6102-2a01-c23-752b-be00-3484-f495-ddc1-446b.ngrok.io/api/v1/') ngrok http 3000
     : (baseURL = 'http://localhost:3000/api/v1/');
 } */
 
